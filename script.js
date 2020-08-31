b79eaef8d799d6d03eda89086fae7521bae977de
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-08-31 23:21:00 +0900

    Fixed unexpected codes

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 chrome.tabs.executeScript({
-    code: 'document.querySelectorAll(\'iframe#cafe_main\')[0].contentWindow.document.body.querySelector("iframe").src'
+    // code: 'document.querySelectorAll(\'iframe#cafe_main\')[0].contentWindow.document.body.querySelector("iframe").src'
+    code: 'document.querySelectorAll("iframe#cafe_main")[0].contentWindow.document.body.querySelector("script.__se_module_data").getAttribute("data-module")'
 }, function (res) {
-    console.log(res[0].split('?')[1].split('&'));
-    search_param = new URLSearchParams(res[0].split('?')[1]);
-    console.log(search_param.get("vid"));
-    console.log(search_param.get("inKey"));
+    res = JSON.parse(res)['data']
+    console.log(res)
+    // console.log(res[0].split('?')[1].split('&'));
+    // search_param = new URLSearchParams(res[0].split('?')[1]);
+    // console.log(search_param.get("vid"));
+    // console.log(search_param.get("inKey"));
+    console.log(res['vid'])
+    console.log(res['inkey'])
 
     let query_string = new URLSearchParams('key=&videoId=')
-    query_string.set('videoId', search_param.get("vid"))
-    query_string.set('key', search_param.get("inKey"))
-    query_string = 'https://apis.naver.com/rmcnmv/rmcnmv/vod/play/v2.0/' + search_param.get("vid") + '?' + query_string.toString()
+    query_string.set('videoId', res['vid'])
+    query_string.set('key', res['inkey'])
+    query_string = 'https://apis.naver.com/rmcnmv/rmcnmv/vod/play/v2.0/' + res['vid'] + '?' + query_string.toString()
+    console.log(query_string)
     fetch_json(query_string)
 });
 
@@ -27,11 +33,11 @@ function fetch_json(url) {
         function (res) {
             console.log(res.json().then(
                 function (res) {
-                    // console.log(res.videos)
-                    // console.log(res.videos.list)
-                    // console.log(res.videos.list.length)
-                    // console.log(res.videos.list[0])
-                    // console.log(res.videos.list[0].source)
+                    console.log(res.videos)
+                    console.log(res.videos.list)
+                    console.log(res.videos.list.length)
+                    console.log(res.videos.list[0])
+                    console.log(res.videos.list[0].source)
 
                     for (let i = 0; i < res.videos.list.length; i++) {
                         let download_link = document.createElement('a');
